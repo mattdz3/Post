c54621d85b87938ef9492b291cdad4da58dffc20
Matthew Dougherty <mattz325@gmail.com>
2014-10-07 19:06:01 -0400

    working sign up

--- app/scripts/SignUpView.js
+++ app/scripts/SignUpView.js
@@ -21,7 +21,22 @@ var SignUpView = Parse.View.extend({
 
 	createUser: function() {
 		var user = new Parse.User();
-		user.set("username", $('.username').val());
-		user.set("password", $('.password').val());
+		user.set("username", $('.username').val())
+		user.set("password", $('.password').val())
+
+		user.signUp(null, {
+			success: function(user) {
+				var currentUser = Parse.User.current();
+				if (currentUser) {
+					router.navigate('home', {trigger: true});
+					console.log(currentUser)
+				} else {
+					console.log("did not sign up")
+				}
+			},
+			error: function(user, error) {
+				alert("Error" + error.code + " " + error.message);
+			}	
+		})
 	},
 })
